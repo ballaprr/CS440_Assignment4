4a02cf29fe9ad79bf54c658915af4115d085b341
N-Atwood <81779036+N-Atwood@users.noreply.github.com>
2023-02-26 20:28:11 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 /*
 Skeleton code for linear hash indexing
 */
-
 #include <string>
 #include <ios>
 #include <fstream>
@@ -17,26 +16,13 @@ using namespace std;
 
 
 int main(int argc, char* const argv[]) {
+    cout << "This programme actually starts at literally any point.";
 
     // Create the index
     LinearHashIndex emp_index("EmployeeIndex");
     emp_index.createFromFile("Employee.csv");
     
     // Loop to lookup IDs until user is ready to quit
-    bool check = true;
-    int x;
-    while (check) {
-        cout << "Tyep 1 to enter id for lookup";
-        cout << "Type 2 to enter number to stop";
-        cout << "Type a number: ";
-        cin >> x;
-        if (x == 1) {
-
-        }
-        else if (x == 2) {
-            check = false;
-        }
-    }
     
 
     return 0;
