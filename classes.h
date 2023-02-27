523c483b6563026224b04f7e95dc2be9b2805109
N-Atwood <81779036+N-Atwood@users.noreply.github.com>
2023-02-27 14:49:52 -0800

    Update classes.h

--- classes.h
+++ classes.h
@@ -46,37 +46,36 @@ public:
 
     // Given the filestream and block address, move to and get a given block.
     void fetchFromFile(fstream& indFile, int blockAddr) {
-        char* temp_block_charp;
+        char temp_block_charp[BLOCK_SIZE];
         indFile.seekg(blockAddr);
         indFile.read(temp_block_charp, BLOCK_SIZE);
+        indFile.seekg(blockAddr);
 
-        std::string temp_block(temp_block_charp);
-
-        std::string numRecordsStr(temp_block, BLOCK_SIZE - 16, 8);
+        std::string numRecordsStr(&temp_block_charp[BLOCK_SIZE - 16], 8);
         numRecords = stoi(numRecordsStr);
         for (int i = 0; i < numRecords; i++) {
             int record_offset = i * 716;
             vector<std::string> fields;
 
-            std::string eid(temp_block, record_offset, 8);
+            std::string eid(&temp_block_charp[record_offset], 8);
             fields.push_back(eid);
-            std::string ename(temp_block, record_offset, 200);
+            std::string ename(&temp_block_charp[record_offset + 8], 200);
             fields.push_back(ename);
-            std::string ebio(temp_block, record_offset, 500);
+            std::string ebio(&temp_block_charp[record_offset + 208], 500);
             fields.push_back(ebio);
-            std::string emid(temp_block, record_offset, 8);
+            std::string emid(&temp_block_charp[record_offset + 708], 8);
             fields.push_back(emid);
 
             Record emp(fields);
-            records[i] = emp;
+            records.push_back(emp);
         }
 
-        std::string nextBufferStr(temp_block, BLOCK_SIZE - 8, 8);
+        std::string nextBufferStr(&temp_block_charp[BLOCK_SIZE - 8], 8);
         nextBuffer = stoi(nextBufferStr);
     }
 
     void insertRecord(Record record) {
-        records[numRecords] = record;
+        records.push_back(record);
         numRecords++;
     }
 
@@ -89,7 +88,7 @@ public:
     // Given the filestream and current block, fetch an overflow block.
     void fetchNext(fstream& indFile) {
         if (nextBuffer) {
-            fetchFromFile(indFile, nextBuffer);
+            fetchFromFile(indFile, nextBuffer * BLOCK_SIZE);
         }
         else {
             cout << "ERR: Attempted to fetch next file without nextBuffer set!";
@@ -100,33 +99,42 @@ public:
     // the passed position in the file.
     void writeBlock(fstream& indFile) {
         // Initialise variables
-        char writeBuffer[BLOCK_SIZE];
+        char writeBuffer[BLOCK_SIZE + 1];
+        memset(writeBuffer, '\0', BLOCK_SIZE);
         int i;
-        char* temp;
-
+        char temp[9];
         // Copy all records into the string
         for (i = 0; i < numRecords; i++) {
+            //cout << "Writing first record...\n";
             int j;
 
             // Copy employee id
+            //cout << "Employee ID:\t";
             snprintf(temp, 9, "%d", records[i].id);
             for (j = 0; j < 8 - strlen(temp); j++) {
-                writeBuffer[i * BLOCK_SIZE + j] = '0';
+                writeBuffer[i * 716 + j] = '0';
+                //cout << '0';
             }
-            strncpy(&writeBuffer[i * BLOCK_SIZE + j], temp, strlen(temp));
+            strncpy(&writeBuffer[i * 716 + j], temp, strlen(temp));
+            //cout << temp << endl;
             
             // Copy employee name
-            records[i].name.copy(&writeBuffer[i * BLOCK_SIZE + 8], 200);
+            records[i].name.copy(&writeBuffer[i * 716 + 8], 200);
+            //cout << "Name:\t" << records[i].name << endl;
 
             // Copy employee bio
-            records[i].bio.copy(&writeBuffer[i * BLOCK_SIZE + 208], 500);
+            records[i].bio.copy(&writeBuffer[i * 716 + 208], 500);
+            //cout << "Bio:\t" << records[i].bio << endl;
 
             // Copy manager id
+            //cout << "Manager ID:\n";
             snprintf(temp, 9, "%d", records[i].manager_id);
             for (j = 0; j < 8 - strlen(temp); j++) {
-                writeBuffer[i * BLOCK_SIZE + j] = '0';
+                writeBuffer[i * 716 + j] = '0';
+                //cout << '0';
             }
-            strncpy(&writeBuffer[i * BLOCK_SIZE + 708 + j], temp, strlen(temp));
+            strncpy(&writeBuffer[i * 716 + 708 + j], temp, strlen(temp));
+            //cout << temp << endl;
         }
 
         snprintf(temp, 9, "%d", numRecords);
@@ -134,12 +142,14 @@ public:
             writeBuffer[BLOCK_SIZE - 16 + i] = '0';
         }
         strncpy(&writeBuffer[BLOCK_SIZE - 16 + i], temp, strlen(temp));
+        //cout << "numRecords:\t" << numRecords << endl;
 
         snprintf(temp, 9, "%d", nextBuffer);
         for (i = 0; i < 8 - strlen(temp); i++) {
             writeBuffer[BLOCK_SIZE - 8 + i] = '0';
         }
         strncpy(&writeBuffer[BLOCK_SIZE - 8 + i], temp, strlen(temp));
+        //cout << "nextBuffer:\t" << nextBuffer << endl;
 
         indFile.write(writeBuffer, BLOCK_SIZE);
     }
@@ -164,6 +174,7 @@ private:
         fstream indexedFile;
         indexedFile.open(fName, ios::out | ios::in);
 
+        /*
         // No records written to index yet
         // This loop uses 2 blocks (one of class Block, one temporary string to write Block)
         if (numRecords == 0) {
@@ -173,16 +184,17 @@ private:
             // Initialize index with first blocks (start with 4)
             for (int iter = 0; iter < 4; iter++) {
                 blockDirectory[iter] = nextFreeBlock;
-                nextFreeBlock += BLOCK_SIZE;
+                nextFreeBlock ++;
 
                 // Place an empty block in the designated spot.
                 Block emptyBlock;
-                indexedFile.seekg(blockDirectory[iter]);
+                indexedFile.seekg(blockDirectory[iter] * BLOCK_SIZE);
                 emptyBlock.writeBlock(indexedFile);
             }
             n = 4;
             i = 2;
         }
+        */
 
         // Add record to the index in the correct block, creating an overflow block if necessary
         // Adding a record to the index uses at most 2 blocks (one of class Block, one temporary string
@@ -196,11 +208,12 @@ private:
         if (sigbits >= n) {
             sigbits = sigbits % int_pow(2, i - 1);
         }
-        int targetBlockAddr = blockDirectory[sigbits];
+        //cout << "Inserting into bucket " << sigbits << endl;
+        int targetBlockAddr = blockDirectory[sigbits] * BLOCK_SIZE;
         Block tempBlock;
+
         tempBlock.fetchFromFile(indexedFile, targetBlockAddr);
         // Get a block with at least one free space
-        // 
         while (tempBlock.numRecords == 5) {
             if (tempBlock.nextBuffer) {
                 tempBlock.fetchNext(indexedFile);
@@ -211,8 +224,8 @@ private:
                 tempBlock.writeBlock(indexedFile);
 
                 // Go to the next block and increment nextFreeBlock.
-                indexedFile.seekg(nextFreeBlock);
-                nextFreeBlock += BLOCK_SIZE;
+                indexedFile.seekg(nextFreeBlock * BLOCK_SIZE);
+                nextFreeBlock++;
 
                 // Clear tempBlock's data.
                 tempBlock.clear();
@@ -221,49 +234,67 @@ private:
         tempBlock.insertRecord(record);
         tempBlock.writeBlock(indexedFile);
 
+        // GOOD TO HERE!!!
+        // REMOVE THIS
+
         // Take neccessary steps if capacity is reached:
 		// increase n; increase i (if necessary); place records in the new bucket that may have been originally misplaced due to a bit flip
-        if (++numRecords >= 0.7 * 5 * n) {
+        numRecords++;
+        if (numRecords >= 0.7 * 5 * n) {
+            int blockTracker;
+            //cout << "Increasing n to " << n + 1 << endl;
             // Increase n; if n is greater than 2^i, increase i
             if (++n > int_pow(2, i)) {
+                //cout << "Increasing i to " << i + 1 << endl;
                 i++;
             }
 
             blockDirectory[n - 1] = nextFreeBlock;
-            nextFreeBlock += BLOCK_SIZE;
+            nextFreeBlock++;
 
             // Place an empty block in the designated spot.
             tempBlock.clear();
-            indexedFile.seekg(blockDirectory[n - 1]);
+            indexedFile.seekg(blockDirectory[n - 1] * BLOCK_SIZE);
             tempBlock.writeBlock(indexedFile);
 
             // Get the bucket that is the new largest bucket with the most significant bit turned off
             int bucketToCheck = (n - 1) % int_pow(2, i - 1);
-            tempBlock.fetchFromFile(indexedFile, blockDirectory[bucketToCheck]);
+            blockTracker = blockDirectory[bucketToCheck];
+            tempBlock.fetchFromFile(indexedFile, blockTracker * BLOCK_SIZE);
             int iter = 0;
-            while (iter < tempBlock.numRecords) {
-                // Grab a record from the bucket being checked
-                Record recordChecked = tempBlock.records[iter];
-                // If that record should go in the new one ...
-                if (recordChecked.id % int_pow(2, i) == n - 1) {
-                    // Erase it from the current bucket
-                    tempBlock.records.erase(tempBlock.records.begin() + iter);
-                    // Decrement numRecords (it will be increased in the next line and we don't want
-                    // to double-count moved records)
-                    numRecords--;
-                    // Insert the record. This will use at most 2 blocks at any given time, plus the
-                    // 1 block (tempBlock) at the current depth. n should never be increased more than
-                    // once per insertion, so no more than 3 blocks should be in memory at any given
-                    // time (1 at depth 0, 2 at depth 1)
-                    cout << "Placing record in new spot...";
-                    insertRecord(recordChecked);
-                    cout << "Done!";
-                }
-                // Increment iter; if there are no more records in this bucket and an overflow bucket
-                // exists, go to that and set iter to 0
-                if (++iter == tempBlock.numRecords && tempBlock.nextBuffer != 0) {
+            while (iter < tempBlock.numRecords || tempBlock.nextBuffer != 0) {
+                if (iter == tempBlock.numRecords) {
+                    blockTracker = tempBlock.nextBuffer;
                     tempBlock.fetchNext(indexedFile);
                     iter = 0;
+                }
+                else {
+                    // Grab a record from the bucket being checked
+                    Record recordChecked = tempBlock.records[iter];
+                    // If that record should go in the new one ...
+                    if (recordChecked.id % int_pow(2, i) == n - 1) {
+                        // Erase it from the current bucket
+                        tempBlock.records.erase(tempBlock.records.begin() + iter);
+                        tempBlock.numRecords--;
+                        tempBlock.writeBlock(indexedFile);
+                        // Decrement numRecords (it will be increased in the next line and we don't want
+                        // to double-count moved records)
+                        numRecords--;
+                        // Insert the record. This will use at most 2 blocks at any given time, plus the
+                        // 1 block (tempBlock) at the current depth. n should never be increased more than
+                        // once per insertion, so no more than 3 blocks should be in memory at any given
+                        // time (1 at depth 0, 2 at depth 1)
+                        //cout << "Placing record with ID " << recordChecked.id << " in new spot...";
+                        indexedFile.close();
+                        insertRecord(recordChecked);
+                        //cout << "Done!\n";
+                        indexedFile.open(fName, ios::in | ios::out);
+                        indexedFile.seekg(blockTracker * BLOCK_SIZE);
+                    }
+                    else {
+                        iter++;
+                    }
+
                 }
             }
             indexedFile.close();
@@ -272,30 +303,26 @@ private:
 
 public:
     LinearHashIndex(string indexedFileName) {
-        cout << "0";
         n = 4; // Start with 4 buckets in index
         i = 2; // Need 2 bits to address 4 buckets
         numRecords = 0;
         nextFreeBlock = 0;
         fName = indexedFileName;
 
-        cout << "1";
-
         // Create your EmployeeIndex file and write out the initial 4 buckets
         // make sure to account for the created buckets by incrementing nextFreeBlock appropriately
         fstream indexedFile;
         indexedFile.open(fName, ios::out | ios::in | ios::trunc);
+        blockDirectory.reserve(256);
         for (int iter = 0; iter < 4; iter++) {
             blockDirectory[iter] = nextFreeBlock;
-            nextFreeBlock += BLOCK_SIZE;
-
+            nextFreeBlock++;
             // Place an empty block in the designated spot.
             Block emptyBlock;
-            indexedFile.seekg(blockDirectory[iter]);
+            indexedFile.seekg(blockDirectory[iter] * BLOCK_SIZE);
             emptyBlock.writeBlock(indexedFile);
+            //cout << "blockDirectory[" << iter << "] = " << blockDirectory[iter] << endl;
         }
-
-        cout << "2";
     }
 
     // Read csv file and add records to the index
@@ -304,10 +331,9 @@ public:
         fstream csv_file;
         csv_file.open(csvFName, ios::in);
 
-        cout << "3";
-
         // For each entry in the csv file, create and insert a record into the index
         std::string line, word;
+        int loop_counter = 1;
         while (getline(csv_file, line, '\n')) {
             vector<std::string> fields;
             stringstream s(line);
@@ -322,12 +348,12 @@ public:
 
             Record emp(fields);
 
-            cout << "4";
-
+            //cout << loop_counter << endl;
+            loop_counter++;
             insertRecord(emp);
         }
 
-        cout << "5";
+        //cout << "Out of insert loop\n";
 
         csv_file.close();
     }
@@ -345,7 +371,8 @@ public:
         }
 
         Block tempBlock;
-        tempBlock.fetchFromFile(indexedFile, blockDirectory[bucketToCheck]);
+        // CHANGE THIS: THIS FETCHES FROM CSV FILE, NOT OUR FORMAT
+        tempBlock.fetchFromFile(indexedFile, blockDirectory[bucketToCheck] * BLOCK_SIZE);
         int iter = 0;
         int found_record = 0;
         while (iter < tempBlock.numRecords) {
@@ -353,8 +380,8 @@ public:
             Record recordChecked = tempBlock.records[iter];
             // If that record's id matches, print it and stop looking.
             if (recordChecked.id == id) {
-                recordChecked.print();
-                found_record = 1;
+                indexedFile.close();
+                return(recordChecked);
             }
             // Increment iter; if there are no more records in this bucket and an overflow bucket
             // exists, go to that and set iter to 0
@@ -363,9 +390,12 @@ public:
                 iter = 0;
             }
         }
-        if (found_record == 0) {
-            cout << "No matching record found!";
-        }
-        indexedFile.close();
+        vector<std::string> fields;
+        fields.push_back("-1\0");
+        fields.push_back("Employee Not Found\0");
+        fields.push_back("Bio Not Found\0");
+        fields.push_back("-1\0");
+        Record emptyRecord(fields);
+        return(emptyRecord);
     }
 };
